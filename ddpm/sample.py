791a1ffd3bf37a716b6f1c2fb0367410ac203b44
Gianluca Sasanelli <gianluca.sasanelli@gmail.com>
2024-09-16 17:25:34 +0200

    Updating readme and sample

--- ddpm/sample.py
+++ ddpm/sample.py
@@ -48,23 +48,24 @@ class ImageGenerator:
             xt = torch.randn(img_shape, device=device)
         else:
             xt = starting_noise
-        
+        time_steps = []
         images = []
         for t in reversed(range(steps)):
             time_tensor = torch.tensor([t] * img_shape[0], device=device)
             if t % interval == 0 or t==0:
                 print(t)
+                time_steps.append(t)
                 images.append(self.process_image(xt[0]))
             xt, _ = self.model.sample_p(xt, time_tensor)
         
-        return images
+        return images, time_steps
 
     def process_image(self, img):
         img = img.clip(0, 1)
         img = img.cpu().numpy()
         return img.transpose(1, 2, 0)
 
-    def create_animation(self, images, steps):
+    def create_animation(self, images,  time_steps):
         
         fig, ax = plt.subplots()
         plt.close()  # Prevents the empty figure from being displayed
@@ -72,20 +73,20 @@ class ImageGenerator:
         def animate(i):
             ax.clear()
             ax.imshow(images[i])
-            ax.set_title(f"Step: {steps - i * interval}")
+            ax.set_title(f"Step: {time_steps[i]}")
             ax.axis('off')
         
-        anim = FuncAnimation(fig, animate, frames=len(images), interval=200, repeat_delay=1000)
+        anim = FuncAnimation(fig, animate, frames=len(images), interval=200, repeat_delay=5000)
         return anim
 
-    def create_image_grid(self,  images,  grid_size=(5, 5)):        
+    def create_image_grid(self,  images,  time_steps, grid_size=(5, 5)):        
         rows, cols = grid_size
         fig, axs = plt.subplots(rows, cols, figsize=(15, 15))
         
         for i, ax in enumerate(axs.flat):
             if i < len(images):
                 ax.imshow(images[i])
-                ax.set_title(f"Step: {steps - (i) * interval}")
+                ax.set_title(f"Step: {time_steps[i]}")
             ax.axis('off')
         
         plt.tight_layout()
@@ -95,23 +96,23 @@ class ImageGenerator:
 generator = ImageGenerator(model)
 interval = 50
 # Get a batch and create starting noise
-x = get_batch("train")
+x = get_batch("val")
 steps = 750
 t = torch.tensor([steps] * x.shape[0], device=device)
 starting_noise,_ = model.sample_q(x, t)
 
 # Create an animation
-images = generator.generate_image((1, 3, 32, 32), interval = interval, steps = steps, starting_noise=starting_noise) 
-# anim = generator.create_animation(
+images, time_steps = generator.generate_image((1, 3, 32, 32), interval = interval, steps = steps, starting_noise=starting_noise) 
+anim = generator.create_animation(images,  time_steps)
 
 # Save the animation
-# anim.save('celeba_generation_animation.gif', writer='pillow', fps=10)
-# print("Animation has been saved as 'celeba_generation_animation.gif'")
+anim.save('celeba_generation_animation.gif', writer='pillow', fps=10)
+print("Animation has been saved as 'celeba_generation_animation.gif'")
 
 # Display the animation (this will work in a Jupyter notebook)
 
 # Create an image grid
-fig = generator.create_image_grid(images = images)
+fig = generator.create_image_grid(images = images, time_steps = time_steps)
 fig.savefig('celeba_generation_grid.png')
 print("Image grid has been saved as 'celeba_generation_grid.png'")
 
